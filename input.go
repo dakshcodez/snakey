ac2e47ef275e1e392997c4232747b93938d9f9c9
dakshcodez <daksharoracodez@gmail.com>
2025-12-24 14:08:03 +0530

    added w a s d controls

--- input.go
+++ input.go
@@ -5,16 +5,16 @@ import (
 )
 
 func HandleInput(g * Game) {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && g.direction != Down {
+	if (ebiten.IsKeyPressed(ebiten.KeyArrowUp) || ebiten.IsKeyPressed(ebiten.KeyW)) && g.direction != Down {
 		g.direction = Up
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && g.direction != Up {
+	if (ebiten.IsKeyPressed(ebiten.KeyArrowDown) || ebiten.IsKeyPressed(ebiten.KeyS)) && g.direction != Up {
 		g.direction = Down
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && g.direction != Right {
+	if (ebiten.IsKeyPressed(ebiten.KeyArrowLeft) || ebiten.IsKeyPressed(ebiten.KeyA)) && g.direction != Right {
 		g.direction = Left
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && g.direction != Left {
+	if (ebiten.IsKeyPressed(ebiten.KeyArrowRight) || ebiten.IsKeyPressed(ebiten.KeyD)) && g.direction != Left {
 		g.direction = Right
 	}
 }
